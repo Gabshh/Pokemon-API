74340483a1330b1f25141be0600ddf2b0c90837b
Gabriel Gomes <gabriielgomess@outlook.com>
2022-03-30 10:54:38 -0300

    menu pokebola adicionado

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+function menuOnClick(){
+    document.getElementById("menu-pokeball").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-closed").classList.toggle("change-closed");
+}
