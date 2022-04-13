8388b98b7dda6daa19264ef2d1b6f3b2ea7c288e
Gabriel Gomes <gabriielgomess@outlook.com>
2022-04-13 12:04:56 -0300

    função fetchPokemon

--- js/main.js
+++ js/main.js
@@ -1,78 +1,77 @@
-"use strict";
+'use strict'
 
-alert("ta safe :)");
+alert('ta safe :)')
 
-const pesquisarTipos = async () => {
-	const url = "https://pokeapi.co/api/v2/type/";
-	const response = await fetch(url);
-	const data = await response.json();
-	const tipo = Object.keys(data.results);
-	return tipo;
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 649;
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+	for (let i = 1; i <= pokemons_number; i++) {
+		await getPokemon(i);
+	}
 };
 
-const pesquisarPokemon = async (nome) => {
-	const url = `https://pokeapi.co/api/v2/pokemon/${nome}/`;
-	const response = await fetch(url);
-	const data = await response.json();
-
-	return data;
+const getPokemon = async id => {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+	const res = await fetch(url);
+	const pokemon = await res.json();
+	createPokemonCard(pokemon);
 };
 
-const criarImg = (imagem) => {
-	const img = document.createElement("img");
-	img.src = imagem;
-	return img;
-};
+function createPokemonCard(pokemon) {
+	const pokemonEl = document.createElement('div');
+	pokemonEl.classList.add('pokemon');
 
-const carregarImagens = async () => {
-	const spritesContainer = document.getElementById("sprites-container");
-	const idPokemon = document.getElementById("id-pokemon").value;
+	const poke_types = pokemon.types.map(type => type.type.name);
+	const type = main_types.find(type => poke_types.indexOf(type) > -1);
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+	const color = colors[type];
+	
+	pokemonEl.style.backgroundColor = color;
 
-	const imagens = await pesquisarPokemon(idPokemon);
+	const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `;
 
-	const tagImagens = imagens.message.map(criarImg);
+	pokemonEl.innerHTML = pokeInnerHTML;
 
-	spritesContainer.replaceChildren(...tagImagens);
-};
+	poke_container.appendChild(pokemonEl);
 
-const carregarTipos = async () => {
 	const lista = document.getElementById("pesquisa-tipo");
-	const tipos = await pesquisarTipos();
 
 	lista.innerHTML = `
                         <option>
-                            ${tipos.join("</option><option>")}
+                            ${type}</option><option>
                         </option>
                       `;
-};
-
-// CRIANDO CARDS
-
-const criarCard = (pokemon) => {
-    
-    const card = document.createElement('div')
-    card.classList.add('card')
-    card.innerHTML = `
-        <div class="card-image-container">
-            <img src="${pokemon.sprite}" alt="Sprite" class="card-image">
-        </div>
-        <span class="card-descricao">
-            ${pokemon.nome}
-        </span>
-    `
-    return card
-}
-
-const carregarCardsPokemons = (pokemons) => {
-    const container = document.querySelector('.card-container')
-    
-	const cards = pokemons.map(criarCard)
-
-    container.replaceChildren(...cards)
 
 }
 
-carregarCardsPokemons(pesquisarPokemon(bulbasaur))
+fetchPokemons();
 
-document.getElementById("pesquisar").addEventListener("click", carregarImagens())
-document.getElementById("pesquisa-tipo").addEventListener("click", carregarTipos())
